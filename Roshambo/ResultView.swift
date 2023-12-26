a1659510e8ca1732afbc52b93aadca1bef3d5e60
lucasnpc <lucasnpc2@outlook.com>
2023-12-26 12:40:44 -0300

    last adjust in the app

--- Roshambo/ResultView.swift
+++ Roshambo/ResultView.swift
@@ -8,21 +8,75 @@
 import SwiftUI
 
 struct ResultView: View {
-    @Binding var result: RPSMatch?
+    @Binding var result: RPSMatch!
     @SwiftUI.Environment(\.presentationMode) var presentationMode
+    
+    @State private var animate = false
     
     var body: some View {
         
+        VStack{
+            Image(imageForMatch(result)).opacity(animate ? 1.0 : 0.2)
+            Text(messageForMatch(result))
+            Spacer().frame(height: 50)
+            Button(action: {presentationMode.wrappedValue.dismiss()}, label: {
+                Text("Play Again")
+            })
+        }.onAppear {
+            withAnimation(Animation.easeInOut(duration: 1.5)){
+                animate.toggle()
+            }
+        }
+    }
+    
+    // MARK: Messages for Match
+    
+    func messageForMatch(_ match: RPSMatch) -> String {
         
-        Text(result?.p1.description ?? "fck")
+        // Handle the tie
+        if match.p1 == match.p2 {
+            return "It's a tie!"
+        }
         
-//        if result.p1 == result.p2 {
-//            Text("It's a tie!")
-//        }
+        // Here we build up the results message "RockCrushesScissors. You Win!" etc.
+        return match.winner.description + " " + victoryModeString(match.winner) + " " + match.loser.description + ". " + resultString(match)
+    }
+    
+    func resultString(_ match: RPSMatch) -> String {
+        return match.p1.defeats(match.p2) ? "You Win!" : "You Lose!"
+    }
+    
+    
+    func victoryModeString(_ gesture: RPS) -> String {
+        switch (gesture) {
+        case .rock:
+            return "crushes"
+        case .scissors:
+            return "cuts"
+        case .paper:
+            return "covers"
+        }
+    }
+    
+    // MARK: Image for Match
+    
+    func imageForMatch(_ match: RPSMatch) -> String {
         
-        Button(action: {presentationMode.wrappedValue.dismiss()}, label: {
-            Text("Play Again")
-        })
+        var name = ""
+        
+        switch (match.winner) {
+        case .rock:
+            name = "RockCrushesScissors"
+        case .paper:
+            name = "PaperCoversRock"
+        case .scissors:
+            name = "ScissorsCutPaper"
+        }
+        
+        if match.p1 == match.p2 {
+            name = "itsATie"
+        }
+        return name
     }
 }
 
